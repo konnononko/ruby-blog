12e8efca26de35e7ecb9387e0bb12f3fc66fb492
Yusuke Konno <konnononko@gmail.com>
2026-05-01 17:21:48 +0900

    test: User model validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without an email" do
+    user = described_class.new(password: "password123")
+
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to be_present
+  end
 end
